a0b8bd57e95a141f62bbb944fca6724f54247244
Mohamed Essid <mohamedessid2200@gmail.com>
2025-05-07 22:10:18 +0100

    Lab 2 - step1 - Update the Book Service

--- LAB1/demo/src/main/java/com/example/demo/service/BookService.java
+++ LAB1/demo/src/main/java/com/example/demo/service/BookService.java
@@ -1,30 +1,28 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
     public List<Book> getAllBooks() {
-        return Arrays.asList(
-            new Book(1L, "The Great Gatsby", "F. Scott Fitzgerald"),
-            new Book(2L, "1984", "George Orwell")
-        );
+        return bookRepository.findAll();
     }
 
     public Book getBookById(Long id) {
-        return getAllBooks().stream()
-            .filter(book -> book.getId().equals(id))
-            .findFirst()
-            .orElse(null);
+        Optional<Book> book = bookRepository.findById(id);
+        return book.orElse(null);
     }
 
-    public Book addBook(Book book) {
-        // In a real application, this would save to a database
-        // For now, we'll just return the book with the ID set
-        return book;
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
     }
 }
